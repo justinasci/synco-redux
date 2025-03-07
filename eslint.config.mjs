c19e127a2ec2b29409a257e035370bb9ee5cb4a8
Justinas Cicėnas <cicenasjustinas@gmail.com>
2025-03-07 10:09:22 +0200

    eslint rules for testing

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,14 @@ import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-    eslint.configs.recommended,
-    tseslint.configs.recommended,
+	eslint.configs.recommended,
+	tseslint.configs.recommended,
+	{
+		files: ['**/*.test.*'],
+		rules: {
+			'no-unused-expressions': 'off', // Useful for assertion libraries
+			'no-console': 'off', // Allow console logs in tests
+			'@typescript-eslint/no-explicit-any': 'off' // Allow `any` in test files
+		}
+	}
 );
