0eb9073fec3c25dd1cf2501f5a4b94fc3deea176
Justinas Cicėnas <cicenasjustinas@gmail.com>
2025-03-24 15:05:26 +0200

    Electron support (#2)

--- lib/main.ts
+++ lib/main.ts
@@ -3,11 +3,25 @@ import { createProxyStoreEnhancer } from './proxyStore/proxyStore';
 import { isProxyReady } from './proxyStore/isProxyReady';
 import { isProxyStore } from './proxyStore/isProxyStore';
 import { immerProxyStoreReducer } from './proxyStore/proxyReducer';
+import { PortMainComms } from './adapters/PortMainComms';
+import { PortProxyComms } from './adapters/PortProxyComms';
+import { ElectronProxyComms } from './adapters/ElectronProxyComms';
+import { ElectronComms } from './adapters/ElectronMainComms';
+import {
+	getSyncoReduxElectronApi,
+	registerSyncoReduxContextBridge
+} from './electronUtils';
 
 export {
 	createMainStoreEnhancer,
 	createProxyStoreEnhancer,
 	immerProxyStoreReducer,
 	isProxyReady,
-	isProxyStore
+	isProxyStore,
+	PortMainComms,
+	PortProxyComms,
+	ElectronProxyComms,
+	ElectronComms,
+	getSyncoReduxElectronApi,
+	registerSyncoReduxContextBridge
 };
